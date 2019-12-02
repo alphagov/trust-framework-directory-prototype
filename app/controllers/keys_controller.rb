bd2785332b61e6581248b234323d32d9439d422f
brenetic <b@brenetic.com>
2019-12-02 12:50:20 +0000

    Return not found if organisation ID does not exist

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -2,9 +2,17 @@ class KeysController < ApplicationController
   def org_private_keys
     org = Organisation.find_by_organisation_id(params[:organisation_id])
 
-    render json: {
-      signing: org.signing_cert.private_key,
-      transport: org.transport_cert.private_key
-    }
+    if org
+      render json: {
+        signing: org.signing_cert.private_key,
+        transport: org.transport_cert.private_key
+      },
+      status: :ok
+    else
+      render json: {
+        error: "No organisation found for #{params[:organisation_id]}"
+      },
+      status: :not_found
+    end
   end
 end
