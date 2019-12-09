70d7904cab508e631da0a1c39c55385da526db74
brenetic <b@brenetic.com>
2019-12-09 16:37:50 +0000

    Add govuk frontend to the admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,28 @@
 class AdminController < ApplicationController
   def index
-    @brokers = Organisation.brokers
-    @idps = Organisation.idps
+    scheme_hash = Organisation.all.group_by(&:scheme)
+    @schemes = {}
+    scheme_hash.each { |scheme, orgs| @schemes[scheme] = orgs.group_by(&:org_type) }
 
     render :index
   end
 
   def delete_selected
-    org_type = ['broker', 'idp'].find { |type| params.keys.include?(type) }
-    Organisation.delete(params[org_type])
+    org = Organisation.find_by_id(params[:org_id])
+
+    if org
+      Organisation.destroy(params[:org_id])
+      flash[:success] = "Deleted #{org.org_type} #{org.name}"
+    else
+      flash[:error] = "Organisation with ID '#{params[:org_id]}' does not exist"
+    end
 
     redirect_to :admin
   end
 
   def delete_by_org_type
     Organisation.where(org_type: params[:organisation_type]).destroy_all
+    flash[:success] = "Deleted all organsiations of type #{params[:organisation_type]}"
 
     redirect_to :admin
   end
