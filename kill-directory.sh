ec4c87db81e176c0d67e04ad380029448d6db8f6
James Howes <james.howes@digital.cabinet-office.gov.uk>
2020-01-03 10:16:29 +0000

    Don't exit script if PID cannot be found
    - If the PID doesnb't exist then we assume the application has already stopped so carry
    on running the rest of the script and remove the pid file.

--- kill-directory.sh
+++ kill-directory.sh
@@ -3,6 +3,6 @@ set -e
 
 if [ -f "./tmp/pids/directory.pid" ]; then
   echo "Killing directory"
-  kill "$(< ./tmp/pids/directory.pid)"
+  kill "$(< ./tmp/pids/directory.pid)" || true
   rm -f ./tmp/pids/directory.pid
 fi
